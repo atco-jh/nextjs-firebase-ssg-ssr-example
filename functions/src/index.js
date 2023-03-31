825f914193bd6f927c05bb8767b2693c95505a48
atco-jh <127229872+atco-jh@users.noreply.github.com>
2023-03-31 10:06:14 -0600

    Update functions index.js

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,13 +1,19 @@
-const { https } = require("firebase-functions");
-const { default: next } = require("next");
+const { https } = require('firebase-functions');
+const { default: next } = require('next');
+const { join } = require('path');
 
+const dev = process.env.NODE_ENV !== 'production';
+
+const directory = join(__dirname, '..', 'next.config.js');
+const nextjsDistDir = join(__dirname, '..', require(directory).distDir);
 const nextjsServer = next({
-  dev: false,
-  conf: { distDir: "../../next" },
+    dev: dev,
+    conf: nextjsDistDir,
 });
 const nextjsHandle = nextjsServer.getRequestHandler();
 
 exports.nextjsFunc = https.onRequest((req, res) => {
-  console.log("File: ");
-  return nextjsServer.prepare().then(() => nextjsHandle(req, res));
+    console.log('req: ', req);
+    console.log('res: ', res);
+    return nextjsServer.prepare().then(() => nextjsHandle(req, res));
 });
